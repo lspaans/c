d6adfed1a14cab09f6bc1cd78c333abbb1304e60
Leon Spaans <lspaans@xs4all.nl>
2014-12-19 00:07:48 +0100

    Make the escapable character a function argument

--- escape/escape.c
+++ escape/escape.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* escape(char* line_in) {
+/* This function escapes ('\') all occurences of the character indicated
+ * by the variable 'c' in the string referenced by 'line_in' and stores
+ * the output in the string 'line_out' and returns a reference to it.
+ */
+
+char* escape(char* line_in, char c) {
     size_t max_length_line_out = strlen(line_in) * 2;
     char* line_out = (char *) malloc(sizeof(char) * max_length_line_out);
     int i;
     int j = 0;
     for(i=0;i<strlen(line_in);i++) {
-        if (*(line_in+i) == 34) {
+        if (*(line_in+i) == c) {
             *(line_out+j) = '\\';
             j++;
         }
@@ -19,8 +24,8 @@ char* escape(char* line_in) {
 }
 
 void main() {
-    char* line_in = "Wo\"ei";
-    char* line_out = escape(line_in);
+    char* line_in = "This is \"a test\"";
+    char* line_out = escape(line_in, '"');
 
     printf("line_out = '%s'\n", line_out);
 }
